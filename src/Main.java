8abe02a967de96231790211054c0d66f3cfab540
Martin <martingalindo3202@gmail.com>
2025-10-05 23:40:09 -0600

    Menu en consola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     // Clase Main para llamar a las clases recursivas
     public static void main(String[] args) {
@@ -10,7 +9,64 @@ public class Main {
         Factorial Ejercicio5 = new Factorial();
         Fibonacci Ejercicio6 = new Fibonacci();
 
+        int menu = 0;
+        System.out.println("Menu");
+        while(menu==0){
+            System.out.println("1. Suma de numeros naturales");
+            System.out.println("2. Imprimir numeros");
+            System.out.println("3. Imprimir numeros inverso");
+            System.out.println("4. Contador");
+            System.out.println("5. Factorial");
+            System.out.println("6. Fibonacci");
+            System.out.println("7. Salir");
+            System.out.println("Ingrese una opcion");
+            menu = new Scanner(System.in).nextInt();
 
+            switch(menu){
+                case 1:
+                    Ejercicio1.llamadaSumaNumerosNaturales();
+                    break;
+                case 2:
+                    Ejercicio2.llamadaImprimirNumeros();
+                    break;
+                case 3:
+                    Ejercicio3.llamadaImprimirNumerosInverso();
+                    break;
+                case 4:
+                    Ejercicio4.llamadaContador();
+                    break;
+                case 5:
+                    Ejercicio5.llamadaFactorial();
+                    break;
+                case 6:
+                    Ejercicio6.llamadaFibonacci();
+                    break;
+                case 7:
+                    menu = 1;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            System.out.println("\n");
+
+            System.out.println("Desea continuar");
+            System.out.println("1. Si");
+            System.out.println("2. No");
+            int decision = 0;
+            decision = new Scanner(System.in).nextInt();
+
+            if(decision==2){
+                menu = 1;
+            }else if(decision==1){
+                menu = 0;
+                System.out.println("\n Menu");
+            }else{
+                System.out.println("Opcion no valida, hasta luego");
+                menu = 1;
+            }
+
+        }
 
 
 
